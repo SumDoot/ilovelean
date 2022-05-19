b0ed0ec8679c74bf11e79a2426c2d42477adc627
SumDoot <98511699+SumDoot@users.noreply.github.com>
2022-05-19 09:27:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ function ilovelean(){
 }
 
 function no(){
-    alert("local-ip:192.168.31.307;     global-ip:89.234.274.215        Locale:N=10.245454214 E=46.45445");
-    window.top.close();
+    alert("local-ip:192.168.31.307");
+    alert("global-ip:89.234.274.215");
+    alert("Locale:N=10.245454214 E=46.45445");
+    
+    while True(){
+        document.body.style.backgroundColor = "red";
+        document.body.style.backgroundColor = "green";
+        document.body.style.backgroundColor = "blue";
+    }
 }
