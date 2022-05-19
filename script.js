2bd839c38215eef1a3d08e549d82239174230c30
SumDoot <98511699+SumDoot@users.noreply.github.com>
2022-05-19 09:45:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,7 @@ function no(){
     alert("local-ip:192.168.31.307");
     alert("global-ip:89.234.274.215");
     alert("Locale:N=10.245454214 E=46.45445");
+    
+    document.body.style.backgroundColor = "#FFFFFF";
+    document.body.style.backgroundColor = "#000000";
 }
