70d7e0f410e6c5e401e61850282b5afc5ad9d34c
SumDoot <98511699+SumDoot@users.noreply.github.com>
2022-05-19 09:36:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,4 @@ function no(){
     alert("local-ip:192.168.31.307");
     alert("global-ip:89.234.274.215");
     alert("Locale:N=10.245454214 E=46.45445");
-    
-    document.body.style.backgroundColor = "red";
-    document.body.style.backgroundColor = "green";
-    document.body.style.backgroundColor = "blue";
 }
